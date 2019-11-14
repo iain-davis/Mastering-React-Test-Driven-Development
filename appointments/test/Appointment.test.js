f004043958255630d28f6890a2b83363125de0cc
Iain E. Davis <iain@iaindavis.name>
2019-11-13 19:14:24 -0700

    Add some stuff

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -1,27 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Appointment} from '../src/Appointment'
+import {Appointment, AppointmentsDayView} from '../src/Appointment'
+
+const render = component => {
+    const container = document.createElement('div')
+    ReactDOM.render(component, container)
+    return container
+}
 
 describe('Appointment', () => {
     it('renders the customer first name', () => {
-        const customer = { firstName: 'Ashley'}
+        const customer = {firstName: 'Ashley'}
 
-        const container = render(<Appointment customer={customer}/>, container)
+        const container = render(<Appointment customer={customer}/>)
 
         expect(container.textContent).toMatch('Ashley')
     })
 
     it('renders another customer first name', () => {
-        const customer = { firstName: 'Jordan'}
+        const customer = {firstName: 'Jordan'}
 
-        const container = render(<Appointment customer={customer}/>, container)
+        const container = render(<Appointment customer={customer}/>)
 
         expect(container.textContent).toMatch('Jordan')
     })
 
-    const render = component => {
-        const container = document.createElement('div')
-        ReactDOM.render(component, container)
-        return container
-    }
+})
+
+describe('AppointmentsDayView', () => {
+    it('renders a div with the right id', () => {
+        const container = render(<AppointmentsDayView appointments={[]}/>)
+
+        expect(container.querySelector('div#appointmentsDayView')).not.toBeNull()
+    })
+
+    it('renders multiple appointments in an ol element', () => {
+        const today = new Date()
+        const appointments = [
+            {startsAt: today.setHours(12, 0)},
+            {startsAt: today.setHours(13, 0)}
+        ]
+        const container = render(<AppointmentsDayView appointments={appointments}/>)
+
+        expect(container.querySelector('ol')).not.toBeNull()
+        expect(container.querySelector('ol').children).toHaveLength(2)
+    })
 })
